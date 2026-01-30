7311a5dedd60151d6996c3881bb05ddf9f6b1499
LavaReaper <mandarmahesh06@gmail.com>
2026-01-30 21:35:28 +0530

    made notecard background darker

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -20,7 +20,7 @@ const NoteCard = ({ note, setNotes }) => {
         }
     }
     return (
-        <Link to={`/api/notes/${note._id}`} className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00ff9d]"
+        <Link to={`/api/notes/${note._id}`} className="card bg-base-300 over:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00ff9d]"
         >
             <div className="card-body">
                 <h3 className="card-title text-base-content">{note.title}</h3>
